d391d046f6ba9e6a515378e210a1ed4c2e36cf8c
15296861560 <lgy-lgy@qq.com>
2019-09-03 17:01:15 +0800

    启动项目

--- src/main/java/com/bms/bms/model/book.java
+++ src/main/java/com/bms/bms/model/book.java
@@ -4,8 +4,12 @@ import lombok.Data;
 
 @Data
 public class book {
-    private Long id;
-    private String name;
-    private Integer type;
-    private Integer status;
+    private Long id;//每本书唯一编号
+    private Long number;//名字相同时用number来区别是不是“同一本”书
+    private String name;//书名
+    private Integer type;// 类型
+    private Integer status;//状态
+    private Long gmtCreate;//入库时间
+    private Long gmtModified;//修改时间
+    private Integer lendCount;//借出次数
 }
